d5b7ab4c20258d1fa062edc756d6c3f5130e7555
dvoikinalex <94dvoikin@gmail.com>
2016-06-22 13:27:48 +0300

    update index.js in pip-webui-tasks(remove relations to test.js)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 var build = require('./build');
-var test = require('./test');
 var samples = require('./samples');
 var publish = require('./publish');
 
 function all() {
     build();
-    test();
     samples();
     publish();
 }
 
 module.exports = {
     build: build,
-    test: test,
     samples: samples,
     publish: publish,
     all: all    
